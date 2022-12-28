843b0dc08ec06d2ed4fda31705b30f6b57cf2a90
Teo <32597645+Sir-Teo@users.noreply.github.com>
2022-12-28 14:00:40 -0800

    successfully made request to spotify api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@ const isPredicted = ref(false);
 const resultDisplay = ref([]);
 
 
-async function predict(url) {
+async function predict() {
   var imageURL = window.sessionStorage.getItem('image-url');
   const musrec = new MusRec();
   console.time('Loading of model');
@@ -26,15 +26,59 @@ async function predict(url) {
     const topK = musrec.getTopKClasses(result, 5);
     console.timeEnd('First prediction Time');
     isPredicted.value = true;
-    var resultElement = ""
+    var resultString = ""
+    var resultDict = {};
     topK.forEach(x => {
-    resultElement += `${x.value.toFixed(3)}: ${x.label}\n`;
+    resultDict[x.label] = x.value.toFixed(3);
+    resultString += `${x.value.toFixed(3)}: ${x.label}\n`;
     resultDisplay.value.push ({value: x.value.toFixed(3),key: x.label});
   });
-    console.log(resultElement);
-
+    window.sessionStorage.setItem('result', JSON.stringify(resultDict));
   }
+}
+</script>
 
+<script>
+
+
+
+async function recommend() {
+  var result = JSON.parse(window.sessionStorage.getItem('result'));
+
+  var client_id = 'deca4b76e5114fdbb97ce87467341145';
+  var client_secret = '93f8fc29f4a3427881a6228835eefd16';
+
+  fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret,
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      }
+  })
+  .then(r => r.json())
+  .then(r => {
+      console.log(r.access_token);
+      window.sessionStorage.setItem('access_token', r.access_token);
+  })
+
+  const endpoint = "https://api.spotify.com/v1/recommendations";
+  const artists = '6sFIWsNpZYqfjUpaCgueju';
+  const danceability = encodeURIComponent('0.9');
+  const token = window.sessionStorage.getItem('access_token');
+
+  fetch(`${endpoint}?seed_artists=${artists}&target_danceability=${danceability}`, {
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+  })
+  .then(response => response.json())
+  .then(({tracks}) => {
+    tracks.forEach(item => {
+      console.log(`${item.name} by ${item.artists[0].name}`);
+    })
+  })
+    
 }
 </script>
 
@@ -42,6 +86,7 @@ async function predict(url) {
 <template>
 <div id="Display Text">
 <button @click="predict" class = "predict-button">predict</button>
+<button @click="recommend" class = "recommend-button">recommend</button>
 <p v-if = "isLoaded">Model Loaded...</p>
 <p v-if = "isPredicted">Displaying Predicted value...</p>
 <p v-if = "resultDisplay.length !=0" v-for="(r, i) in resultDisplay" :key="i">
@@ -53,9 +98,22 @@ async function predict(url) {
 
 <style scoped>
 .predict-button {
-  width: 60px;
+  width: 100px;
   height: 50px;
   background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 3px 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.recommend-button {
+  width: 100px;
+  height: 50px;
+  background-color: red;
   border: none;
   color: white;
   padding: 3px 5px;
